305341adbaea9556fccce468b7f9728201b2442b
yanaymalik <44392541+yanaymalik@users.noreply.github.com>
2018-11-25 19:51:18 +0200

    car
    homework

---
+++ tesk(car)/tesk(car)/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tesk_car_
+{
+    class Program
+    {
+        class Car
+        {
+            private int carspeed;
+            public int Carspeed
+            {
+                get { return carspeed; }
+                set { carspeed = value; }
+            }
+            private string company;
+            public string Company
+            {
+                get { return company; }
+                set
+                {
+                    if (value == "1")
+                    {
+                        company = "Mercedes"; 
+                    }
+                    if (value == "2")
+                    {
+                        company = "B.M.W.";
+                    }
+                    if (value == "3")
+                    {
+                        company = "ford";
+                    }
+                    ; }
+            }
+            private string color;
+
+            public string Color
+            {
+                get { return color; }
+                set {
+                    if (value == "1")
+                    {
+                        color = "Red";
+                    }
+                    if (value == "2")
+                    {
+                        color = "Blue";
+                    }
+                    if (value == "3")
+                    {
+                        color = "yellow";
+                    }; }
+            }
+
+
+            public void printcar()
+            {
+                Console.WriteLine($"Car details \n company: {company}\n   color:{color}\n  speed:{carspeed}\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string choice;
+            Car car1 = new Car();
+            Console.WriteLine("Please Select a car company \n 1 - Mercedes \n 2 - B.M.W. \n 3 - ford");
+            car1.Company = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Please Select a car color \n 1 - Red \n 2 - Blue \n 3 - yellow");
+            car1.Color = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Please enter car speed");
+            car1.Carspeed = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            car1.printcar();
+            Console.WriteLine("To speed ud press + \n to slow press - \n to stop the car press 0");
+            choice = Console.ReadLine();
+            while (choice != "+" && choice != "-" && choice != "0")
+            {
+                Console.WriteLine("Please enter again + / - / 0\n");
+                choice = Console.ReadLine();
+            }
+            Console.Clear();
+            if (choice == "+")
+            {
+                car1.Carspeed += 1;
+                car1.printcar();
+            }
+            else if(choice == "-")
+            {
+                car1.Carspeed -= 1;
+                car1.printcar();
+            }
+            else if (choice == "0")
+            {
+                car1.Carspeed = 0;
+                car1.printcar();
+                Console.WriteLine("your car stop\n");
+            }
+            car1.Carspeed -= 2;
+            car1.printcar();
+            car1.Carspeed = 0;
+            car1.printcar();
+            Console.WriteLine("your car stop");
+        }
+    }
+}
---
+++ tesk(cookie)/tesk(cookie)/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace tesk_cookie_
+{
+    
+    class Program
+    {
+        class cookie
+        {
+            private string gluten;
+            public string Gluten
+            {
+                get { return gluten; }
+                set
+                {
+                    if (value == "1")
+                    {
+                        gluten = "With gluten";
+                    }
+                    else if (value == "0")
+                    {
+                        gluten = "Gluten free";
+                    }; }
+            }
+            private int eggs;
+            public int Eggs
+            {
+                get { return eggs; }
+                set { eggs = value; }
+            }
+            private double sugar;
+            public double Sugar
+            {
+                get { return sugar; }
+                set { sugar = value; }
+            }
+           
+            private double flour;
+            public double Flour
+            {
+                get { return flour; }
+                set { flour = value; }
+            }
+            public void printcookie()
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine($"  -*-*-*cookie {i + 1} *-*-*-");
+                    Console.WriteLine($"The cookie contains:\n"
+                    + $"Gluten: {gluten} \n"
+                    + $"Eggs: {Eggs} \n"
+                    + $"Sugar: {Sugar} (grams)\n"
+                    + $"Flour: {Flour} (grams)\n"
+                    );
+                }
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            cookie[] listCookies = new cookie[3];
+            for (int i = 0; i < 3; i++)
+                listCookies[i] = new cookie();
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Clear();
+                Console.WriteLine($"-*-*-*  Choose what will be in the cookie number {i + 1}  *-*-*-");
+                Console.WriteLine("press 0 gluten free \npress 1 With gluten ");
+                listCookies[i].Gluten = Console.ReadLine();
+                
+                Console.WriteLine("How many eggs?");
+                listCookies[i].Eggs = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("how much sugar? (grams)");
+                listCookies[i].Sugar = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("how much flour? (grams)");
+                listCookies[i].Flour = Convert.ToDouble(Console.ReadLine());
+                Console.Clear();
+            }
+            listCookies[0].printcookie();
+            Console.ReadKey();
+            Console.Clear();
+            listCookies[0].Sugar = 4.15;
+            listCookies[0].printcookie();
+            Console.ReadKey();
+            Console.Clear();
+            listCookies[0] = listCookies[1];
+            listCookies[1].Eggs = 2;
+            listCookies[0].printcookie();
+            Console.ReadKey();
+        }
+    }
+}
