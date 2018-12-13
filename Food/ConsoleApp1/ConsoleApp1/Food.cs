19b124cb8d04f4a90e5c744f60cbc8a719fa5fee
yanaymalik <44392541+yanaymalik@users.noreply.github.com>
2018-12-13 15:30:03 +0200

    Cinema food menu

---
+++ Food/ConsoleApp1/ConsoleApp1/Food.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Food
+    {
+        //Seperate menu
+        public const string partition = "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*";
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int price;
+
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        private Boolean kosher;
+
+        public Boolean Kosher
+        {
+            get { return kosher; }
+            set { kosher = value; }
+        }
+        private string maker_name;
+
+        public string Maker_name
+        {
+            get { return maker_name; }
+            set { maker_name = value; }
+        }
+        private int expiration_year;
+
+        public int Expiration_year
+        {
+            get { return expiration_year; }
+            set { expiration_year = value; }
+        }
+        //constructor
+        public Food(string name, int price, Boolean kosher, string manufacturer_name, int expiration_year)
+        {
+            Name = name;
+            Price = price;
+            Kosher = kosher;
+            Maker_name = manufacturer_name;
+            Expiration_year = expiration_year;
+        }
+        //Expiry date checking function
+        public string ex_date()
+        {
+            DateTime todaysDate = DateTime.Now.Date;
+            int year = todaysDate.Year;
+            string ex = year >= expiration_year ? $"The {Name} Expired date is good" : $"The {Name} has expired";
+            return ex;
+        }
+        //Kosher checking function
+        public string if_kosher()
+        {
+            string k = kosher == true ? "The food is kosher" : "The food is not kosher";
+            return k;
+        }
+        //Menu printing function
+        public void Product_info()
+        {
+            string date = $" Product: {name}\n price: {price}\n {if_kosher()}\n Maker: {maker_name}\n {ex_date()}";
+            Console.WriteLine(date);
+            Console.WriteLine(partition);
+        }
+
+    }
+}
---
+++ Food/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //size of arr
+        public const int SIZE = 5;
+        static void Main(string[] args)
+        {
+            //Create + Fill objects
+            string[] Name_list = new string[] { "Ice cream", "Chocolate", "Popcorn", "Fries", "Nachos" };
+            int[] Price_list = new int[] { 5, 10, 13, 15, 16 };
+            Boolean[] ifKosher = new Boolean[] { true, false, true, false, true };
+            string[] manufacturer_List = new string[] { "Tnuva", "Nestle", "Strauss", "lay's", "Osem" };
+            int[] Expiration_list = new int[] { 2018, 2017, 2019, 2017, 2018 };
+            Food[] food = new Food[SIZE];
+            for (int i = 0; i < SIZE; i++)
+                food[i] = new Food(Name_list[i], Price_list[i], ifKosher[i],
+                                       manufacturer_List[i], Expiration_list[i]);
+            //Food menu Cinema
+            Console.WriteLine("*-*-*-*- Cinema food menu -*-*-*-*");
+            //Print the menu
+            for (int i = 0; i < SIZE; i++)
+                food[i].Product_info();
+        }
+    }
+}
